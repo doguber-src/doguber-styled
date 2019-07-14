7c1937a46ecf59c6024204f00260c7b0553e99cb
deivid <deividmflinhares2@gmail.com>
2019-07-14 16:28:50 -0300

    added constants

---
+++ constants/Colors.js
@@ -0,0 +1,13 @@
+const primaryColor = '#CCCCCC';
+const secondaryColor = '#666666';
+const disabledColor = '#999999';
+const primaryBlueColor = '#206585';
+const secondaryBlueColor = '#4c839d';
+
+export default {
+  primaryColor,
+  secondaryColor,
+  disabledColor,
+  primaryBlueColor,
+  secondaryBlueColor,
+};
