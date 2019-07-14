5c36ff23569a8cbf970eb7d6fd715dfa59c9d15a
deivid <deividmflinhares2@gmail.com>
2019-07-14 17:05:51 -0300

    fix package

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import Container from './Container';
+import Row from './Row';
+import Col from './Col';
+import Nav from './Nav';
+import Navbar from './Navbar';
+import NavbarBrand from './NavbarBrand';
+import NavbarToggler from './NavbarToggler';
+import NavItem from './NavItem';
+import NavLink from './NavLink';
+import Collapse from './Collapse';
+import Image from './Image';
+
+export {
+  Container,
+  Row,
+  Col,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  Collapse,
+  Image,
+};
