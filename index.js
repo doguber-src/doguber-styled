e95637c75adb4631e15f750fdfd20108744f1066
deivid <deividmflinhares2@gmail.com>
2019-07-14 16:29:34 -0300

    components index export

---
+++ index.js
@@ -0,0 +1,25 @@
+import Container from './src/Container'
+import Row from './src/Row';
+import Col from './src/Col';
+import Nav from './src/Nav';
+import Navbar from './src/Navbar';
+import NavbarBrand from './src/NavbarBrand';
+import NavbarToggler from './src/NavbarToggler';
+import NavItem from './src/NavItem';
+import NavLink from './src/NavLink';
+import Collapse from './src/Collapse';
+import Image from './src/Image';
+
+export {
+  Container,
+  Row,
+  Col,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  Collapse,
+  Image,
+};
