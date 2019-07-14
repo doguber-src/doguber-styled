72cbadecd8db273a4ef9804dba92004ba5c01947
deivid <deividmflinhares2@gmail.com>
2019-07-14 19:19:15 -0300

    fix babel

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                     {
                         loader: "babel-loader",
                         options: {
-                            "presets": [["es2015", { "modules": false }], "react"]
+                            "presets": [["env", { "modules": false }], "react"]
                         }
                     }
                 ]
