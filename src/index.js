f99217cd9506c72f408dacdadff13336af0886cf
deivid <deividmflinhares2@gmail.com>
2019-07-16 10:21:20 -0300

    version 1.9.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Container from './Container';
 import Row from './Row';
-// import Col from './Col';
+import Col from './Col';
 import Nav from './Nav';
 import Navbar from './Navbar';
 import NavbarBrand from './NavbarBrand';
@@ -13,7 +13,7 @@ import Image from './Image';
 export {
   Container,
   Row,
-  // Col,
+  Col,
   Nav,
   Navbar,
   NavbarBrand,
